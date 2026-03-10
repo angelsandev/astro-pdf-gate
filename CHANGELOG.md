98eb4c8745185bafc55559d1eec7d58cf1d05ca8
Angel Sanchez <angsangui2@alu.edu.gva.es>
2026-03-10 12:41:55 +0100

    ### [V0.0.6] Filtrar Datos  - 2026-3-10 - **Pruebas:** ✅ Todo ok. - **Pruebas:** ✅ Crear un archivo con todos los datos de los documentos. Acceder a ese archivo desde cada componente y filtrar según el tipo. - **Pruebas:** ✅ Probar cambio de `Descargas` a `Fichas de Datos` y `Manuales` y comprobar que se muestran todos los datos y Cards correctos. - **Estructura:** Crear `/data/documentos.js` que contiene la lista con todos los documentos de todos los tipo posibles, sin filtrar. - **manuales.astro:** Importar el array de datos y hacer un `filter` para filtrar por tipo Manual. - **fichas.astro:** Importar el array de datos y hacer un `filter` para filtrar por tipo Ficha de Datos. - **index.astro:** Importar el array de datos y no filtrar, así mostrará todos los documentos. - **imagenes:** Mejora en el diseño CSS de las imagenes para optimizar rendimiento.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,20 @@
 # Astro PDF GATE: Descarga de PDFs con validación por email
 
+### [V0.0.6] Filtrar Datos  - 2026-3-10
+- **Pruebas:** ✅ Todo ok. 
+- **Pruebas:** ✅ Crear un archivo con todos los datos de los documentos. Acceder a ese archivo desde cada componente y filtrar según el tipo.
+- **Pruebas:** ✅ Probar cambio de `Descargas` a `Fichas de Datos` y `Manuales` y comprobar que se muestran todos los datos y Cards correctos.
+- **Estructura:** Crear `/data/documentos.js` que contiene la lista con todos los documentos de todos los tipo posibles, sin filtrar.
+- **manuales.astro:** Importar el array de datos y hacer un `filter` para filtrar por tipo Manual.
+- **fichas.astro:** Importar el array de datos y hacer un `filter` para filtrar por tipo Ficha de Datos.
+- **index.astro:** Importar el array de datos y no filtrar, así mostrará todos los documentos.
+- **imagenes:** Mejora en el diseño CSS de las imagenes para optimizar rendimiento.
+
+
 ### [V0.0.5] Fichas de Datos  - 2026-3-10
 - **Pruebas:** ✅ Todo ok. 
 - **Pruebas:** ✅ Probar cambio de `Descargas` a `Fichas de Datos` y comprobar que se muestran todos los datos y Cards correctos.
 - **Estructura:** Crear `fichas.astro` para introducir listas con datos de tipo, tamaño y url para cada pdf y el tipo será = `Fichas de Datos`.
-
-
-
 
 
 ### [V0.0.4] PdfCard-Component y manuales.astro  - 2026-3-10
--- src/data/documentos.js
+++ src/data/documentos.js
@@ -1,1 +1,50 @@
-
+export const dataPdfs = [
+  {
+    title: "Título documento 1",
+    type: "Manual",
+    size: "300KB",
+    url: "/pdfs/UP5.2. Creación de usuarios para acceso remoto al servidor FTP - CE4.b).pdf",
+  },
+  {
+    title: "Título documento 2",
+    type: "Manual",
+    size: "120KB",
+    url: "/pdfs/UP5.3. Acceso al servidor en modo activo y pasivo - CE4.c).pdf",
+  },
+  {
+    title: "Título documento 3",
+    type: "Manual",
+    size: "320KB",
+    url: "/pdfs/UP5.2. Creación de usuarios para acceso remoto al servidor FTP - CE4.b).pdf",
+  },
+  {
+    title: "Título documento 4",
+    type: "Manual",
+    size: "120KB",
+    url: "/pdfs/UP5.3. Acceso al servidor en modo activo y pasivo - CE4.c).pdf",
+  },
+  {
+    title: "Título documento 1",
+    type: "Ficha de Datos",
+    size: "300KB",
+    url: "/pdfs/UP5.2. Creación de usuarios para acceso remoto al servidor FTP - CE4.b).pdf",
+  },
+  {
+    title: "Título documento 2",
+    type: "Ficha de Datos",
+    size: "120KB",
+    url: "/pdfs/UP5.3. Acceso al servidor en modo activo y pasivo - CE4.c).pdf",
+  },
+  {
+    title: "Título documento 3",
+    type: "Ficha de Datos",
+    size: "320KB",
+    url: "/pdfs/UP5.2. Creación de usuarios para acceso remoto al servidor FTP - CE4.b).pdf",
+  },
+  {
+    title: "Título documento 4",
+    type: "Ficha de Datos",
+    size: "120KB",
+    url: "/pdfs/UP5.3. Acceso al servidor en modo activo y pasivo - CE4.c).pdf",
+  },
+];
